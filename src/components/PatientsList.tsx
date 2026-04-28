0cf231bbe5dc9a6574c9acec2cbbd4f32978c8fe
Elise Frazier <elisefraz11@gmail.com>
2026-04-28 15:44:48 -0400

    updated patients list list button

--- src/components/PatientsList.tsx
+++ src/components/PatientsList.tsx
@@ -1,24 +1,33 @@
-import { Avatar, Card, List, ListItem, ListItemText } from "@mui/material";
+import { Avatar, Card, List, ListItem, ListItemText, ListItemButton } from "@mui/material";
 import React from "react";
 import { Patient } from "../types/patient";
 interface PatientsProps {
   patients: any[];
 }
 const PatientsList: React.FC<PatientsProps> = ({ patients }) => {
-let windowHeight = useWindowDimensions()
-let cardHeight = windowHeight.height - 1
+  let windowHeight = useWindowDimensions()
+  let cardHeight = windowHeight.height - 1
 
   return (
-    <Card sx={{width:367,height: `calc(100dvh - 72px - 32px)`,minHeight:'1000px', overflowY:'auto'}}>
+    <Card sx={{ width: 367, height: `calc(100dvh - 72px - 32px)`, minHeight: '1000px', overflowY: 'auto' }}>
       <List>
         {patients.map((patient) => (
-          <ListItem key={patient.name}>
-            <Avatar alt={patient.name} src={patient.profile_picture} />
-            <ListItemText
-            primary={patient.name}
-            secondary={`${patient.gender} ${patient.age}`}
-            />
-          </ListItem>
+          <ListItemButton
+            key={patient.name}
+            sx={{
+              '&:hover': {
+                backgroundColor: '#D8FCF7', // Theme-based or custom color like '#f0f0f0'
+              }
+            }}
+          >
+            <ListItem>
+              <Avatar alt={patient.name} src={patient.profile_picture} />
+              <ListItemText
+                primary={patient.name}
+                secondary={`${patient.gender} ${patient.age}`}
+              />
+            </ListItem>
+          </ListItemButton>
         ))}
       </List>
     </Card>
